23ad0b902f93147c079ff5b1afb1a62f10e4ae10
naopeke <naopeke@protonmail.com>
2023-11-20 10:58:57 +0100

    working with DOM

--- AgenciaDeViaje/versionFinal.js
+++ AgenciaDeViaje/versionFinal.js
@@ -1,65 +1,48 @@
-/* ----request botton---*/
+let customerData = [];
 
-//array for info
-let customersInfo = [];
+//request button
 
-    //funcion　requested info
-function requestedInfo(){
-    let customerName = document.getElementById('name').value;
-    let familyName= document.getElementById('familyName').value;
-    let pointOfOrigin = document.getElementById('pointOfOrigin').value;
-    let destination = document.getElementById('destination').value;
-    let numberOfGuest = document.getElementById('numberOfGests').value;
-    let dateIn = document.getElementById('dateIn').value;
-    let dateOut = document.getElementById('dateOut').value;
-    return{
-        customerName,
-        familyName,
-        pointOfOrigin,
-        destination,
-        numberOfGuest,
-        dateIn,
-        dateOut
-    };
+function requestedInfo (){
+    const firstName = document.getElementById('firstName').value;
+    const familyName = document.getElementById('familyName').value;
+    const pointOfOrigin = document.getElementById('pointOfOrigin').value;
+    const destination = document.getElementById('destination').value;
+    const numberOfGuests = document.getElementById('numberOfGuests').value;
+    const dateIn = document.getElementById('dateIn').value;
+    const dateOut = document.getElementById('dateOut').value;
+
+ let newInfo = {
+  firstName: firstName,
+  familyName: familyName,
+  pointOfOrigin: pointOfOrigin,
+  destination: destination,
+  numberOfGuests: numberOfGuests,
+  dateIn: dateIn,
+  dateOut: dateOut
+ };
+
+  customerData.push(newInfo);     
+  console.log('Newly added info:', newInfo);
+  console.log('List of Info:', customerData);
 }
 
-//request button click//
-document.getElementById('request').addEventListener('click', function (){
-    let newInfo = requestedInfo();
-
-//change requestInfo to JSON stringify
-let newJsonString = JSON.stringify(newInfo);
-customersInfo.push(newJsonString);
-console.log(customersInfo);
-});
 
 
-
-
-
-/*---filter button click---*/
-document.getElementById('filter').addEventListener('click', function (){
-    let filtered = filteredInfo();
-    console.log(filtered);
-});
-
-
+//filter button
 function filteredInfo(){
-    let filteredRequest = customersInfo.filter(person => {
-        //change to lower-case
-        let destinationLowercase = person.destination.toLowerCase();
-        let result = false;
-        if (
-            person.destination.toLowerCase() === 'canarias' ||
-            person.destination.toLowerCase() === 'mallorca' ||
-            person.destination.toLowerCase() === 'galicia'
-        ){
-         result = true;
-         console.log(result);
-        }
-        return result;
-    });
-    return filteredRequest;
+  
 }
 
 
+
+
+// document.addEventListener('DOMContentLoaded', () => 
+// { // everything put in here to let it be able to update DOM after DOM is initialized
+//     const searchContainer = document.querySelector('#search-form')
+
+// window.addEventListener('DOMContentLoaded', function() {
+//   const request = document.getElementById('request')
+//   const filter = document.getElementById('filter')
+//     console.log('ページが読み込まれました！');
+  
+// });
